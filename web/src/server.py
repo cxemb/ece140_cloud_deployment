55a92c254209a55b1e5124c0900885d450611df1
cxemb <74472005+cxemb@users.noreply.github.com>
2021-05-17 10:13:20 -0700

    cv

--- web/src/server.py
+++ web/src/server.py
@@ -1,9 +1,15 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.renderers import render_to_response
+from pyramid.response import Response
 
+import pyramid.httpexceptions as exc
 import mysql.connector as mysql
 import os
+#import requests
+import json
+
+GUEST_DB_FILE_PATH = './guest_database.txt'    # Location of guest DB relative to server.py
 
 db_user = os.environ['MYSQL_USER']
 db_pass = os.environ['MYSQL_PASSWORD']
@@ -11,14 +17,21 @@ db_name = os.environ['MYSQL_DATABASE']
 db_host = os.environ['MYSQL_HOST']
 
 def get_home(req):
-  # Connect to the database and retrieve the users
+  # Connect to the database and retrieve the guests
   db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
   cursor = db.cursor()
   cursor.execute("select first_name, last_name, email from Users;")
   records = cursor.fetchall()
   db.close()
 
-  return render_to_response('templates/home.html', {'users': records}, request=req)
+  return render_to_response('templates/home.html', {'guests': records}, request=req)
+
+pages = ["cv"]
+
+def cv(req):
+  pg_id = int(req.matchdict['pages'])
+  data = {'pages':pages[pg_id]}
+  return render_to_response('templates/cv.html', data, request=req)
 
 ''' Route Configurations '''
 if __name__ == '__main__':
@@ -35,3 +48,10 @@ if __name__ == '__main__':
   app = config.make_wsgi_app()
   server = make_server('0.0.0.0', 6000, app)
   server.serve_forever()
+
+# Route to add guests
+ #  config.add_route('add_guest', '/add_guest')
+ # config.add_view(add_guest, route_name='add_guest')
+
+  config.add_route('get_home', '/')
+  config.add_view(get_home, route_name='get_home')
